412db1e4f5b126218ae94464e925e27442ac472b
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-08 14:14:29 +0300

    Init commit.

---
+++ src/test/java/merkanto/sdjpahibernatedao/SdjpaHibernateDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.sdjpahibernatedao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaHibernateDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
